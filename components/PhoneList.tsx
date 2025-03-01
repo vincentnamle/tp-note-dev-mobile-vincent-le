392651d39968ce10f019bbb4016fe36c2aa05fc9
vincentle <vincent.le57@icloud.com>
2025-03-01 10:15:29 +0100

    Update PhoneList.tsx

--- components/PhoneList.tsx
+++ components/PhoneList.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
 import { NavigationProp } from "@react-navigation/native";
 import { Phone } from "../models/model";
 import { useRecoilValue } from "recoil";
@@ -21,12 +21,19 @@ export default function PhoneList({ navigation }: Props) {
   );
 
   return (
-    <View>
-      <Text style={{ margin: 10 }}>
+    <View style={tw`flex-1 p-4`}>
+      <Text style={tw`p-3 mb-3`}>
         Nombre d'annonces : {filteredPhones.length}
       </Text>
+
+      <TextInput
+        style={tw`h-10 border mx-3 mb-3 px-3 rounded`}
+        placeholder="Rechercher un téléphone..."
+        value={query}
+        onChangeText={setQuery}
+      />
       <FlatList
-        data={phones}
+        data={filteredPhones}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <PhoneItem phone={item} />}
       />
