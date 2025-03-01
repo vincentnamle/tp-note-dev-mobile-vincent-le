2c1b61dd492856c77769ee8ba5c6fb556ce3a3b4
vincentle <vincent.le57@icloud.com>
2025-03-01 09:20:29 +0100

    models

---
+++ models/model.ts
@@ -0,0 +1,15 @@
+export class Phone {
+    public id: string;
+    public model: string;
+    public brand: string;
+    public os: string;
+    public releaseDate: Int16Array;
+    public salerAvatar: string;
+    public saler: string;
+    public description: string;
+    public saleGender: string;
+    public salerCity: string;
+    public salerCountry: string;
+    public phone: string;
+    public price: number;
+}
